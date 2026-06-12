6eb1072c9c9462c540f5b79811d33584cb26a13c
Muhammad Nouman Ali <mnouman101091@gmail.com>
2026-06-12 18:10:50 +0500

    Work on table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 import Navbar from './components/Navbar'
+import Tablelist from './components/Tablelist'
+import ModalForm from './components/ModalForm'
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalMode, setModalMode] = useState("add");
+
+  const handleIsOpen = (mode) => {
+    setIsOpen(true);
+  }
+
+  const handleSubmit = () => {
+    if (modalMode === 'add') {
+      console.log("modal mode add");
+    } else {
+      console.log("modal mode edit");
+    }
+  }
+
   return (
     <>
       <Navbar />
+      <Tablelist />
+      <ModalForm />
     </>
   )
 }
--- client/src/components/Tablelist.jsx
+++ client/src/components/Tablelist.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Tablelist = () => {
+
+    const clients = [
+        { id: 1, name: "John Doe", email: "johndoe@gmail.com", job: "developer", rate: "100", isActive: true },
+        { id: 2, name: "John1 Doe", email: "johndoe1@gmail.com", job: "developer", rate: "101", isActive: true },
+        { id: 3, name: "John2 Doe", email: "johndoe2@gmail.com", job: "developer", rate: "102", isActive: false }
+    ]
+
     return (
         <>
             <div className="overflow-x-auto">
@@ -10,32 +17,30 @@ const Tablelist = () => {
                         <tr>
                             <th></th>
                             <th>Name</th>
+                            <th>Email</th>
                             <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Rate</th>
+                            <th>Status</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="hover:bg-base-300">
                         {/* row 1 */}
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr className="hover:bg-base-300">
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
+                        {clients.map((client) => (
+                            <tr>
+                                <th>{client.id}</th>
+                                <td>{client.name}</td>
+                                <td>{client.email}</td>
+                                <td>{client.job}</td>
+                                <td>{client.rate}</td>
+                                <td>
+                                    <button className={`btn rounded-full w-25 ${client.isActive ? `btn-primary` : `btn-outline btn-primary`}`}>
+                                        {client.isActive ? `Active` : `InActive`}
+                                    </button>
+                                </td>
+                                <td><button className='btn btn-secondary w-15'>Update</button></td>
+                                <td><button className='btn btn-accent w-15'>Delete</button></td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
