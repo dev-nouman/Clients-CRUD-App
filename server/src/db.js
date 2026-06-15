6ee8972ea12e0337c6e969367d6eb0060fd06c99
Muhammad Nouman Ali <mnouman101091@gmail.com>
2026-06-15 16:00:59 +0500

    backend complete

--- server/src/db.js
+++ server/src/db.js
@@ -1,9 +1,11 @@
 import pg from "pg";
 import env from "dotenv";
 
-const db = new pgClient({
+env.config();
+
+const db = new pg.Client({
   user: process.env.PG_USER,
-  host: process.env.PG.HOST,
+  host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,19 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import clientRoutes from "./routes/clientRoute.js";
 
 const app = express();
+const port = 3000;
 
-app.use(cors()); 
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: "CORS is working with ES Modules!" });
+app.get("/api", (req, res) => {
+  res.json({ message: "BASE URL" });
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.use("/api", clientRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
